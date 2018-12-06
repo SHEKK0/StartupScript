e33f31708e12b721b78c59eba82dbad797460a1b
SHEKKO <javidm1999@gmail.com>
2018-12-06 13:24:58 +0100

    hola

--- startup.sh
+++ startup.sh
@@ -6,5 +6,5 @@ mv  ~/Downloads/optimus.conf /etc/X11/xorg.conf.d/
 sudo rm /etc/modprobe.d/mhwd*
 mv ~/Downloads/nvidia.conf /etc/modprobe.d/
 mv ~/Downloads/nvidia-drm.conf /etc/modprobe.d/
-mv ~/Downloads/Xsetup /usr/share/sddm/scripts/Xsetup
+mv ~/Downloads/Xsetup /usr/share/sddm/scripts/
 echo "Todo finished"
