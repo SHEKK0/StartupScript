de219ff36d9a189964a8a869d3f7e88a17e571e3
SHEKKO <javidm1999@gmail.com>
2018-12-06 13:13:39 +0100

    new file

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo pacman -Syu
+sudo pacman -S vim
+sudo pacman -S yaourt 
+mv  ~/Downloads/optimus.conf /etc/X11/xorg.conf.d/
+sudo rm /etc/modprobe.d/mhwd*
+mv ~/Downloads/nvidia.conf /etc/modprobe.d/
+mv ~/Downloads/nvidia-drm.conf /etc/modprobe.d/
+mv ~/Downloads/Xsetup /usr/share/sddm/scripts/Xsetup
+echo "Todo finished"
